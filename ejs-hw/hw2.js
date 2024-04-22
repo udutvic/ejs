6ced845141ddbf434a27430d8769284780a3b602
udutvic <102656241+udutvic@users.noreply.github.com>
2024-04-22 11:00:22 +0200

    Add files via upload

---
+++ ejs-hw/hw2.js
@@ -0,0 +1,18 @@
+// Оголошення змінних і їх ініціалізація
+
+let batterConsistency = "medium"; //string
+let patelniaTemperature = 180; //number
+let cookingTime = 1.5; //number float
+let flipCount = 2; //number 
+let ingredientList = ["flour", "eggs", "milk", "sugar"]; // array
+let batterVolume = 500; //number
+let servingSize = "40cm"; //string
+let toppingChoice = {
+    sweety: "chocolate",
+    cheesy: "mozzarella",
+    fruity: "strawberry"       
+}; //object
+let remainingBatter = true; //boolean
+let patelniaOilLevel = "0.75l"; //string
+
+
