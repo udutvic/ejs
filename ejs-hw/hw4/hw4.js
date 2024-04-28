b98d3839df2f88c549d4b134932a44687058940f
udutvic <102656241+udutvic@users.noreply.github.com>
2024-04-28 15:22:15 +0200

    Add files via upload

---
+++ ejs-hw/hw4/hw4.js
@@ -0,0 +1,18 @@
+let integerNumber = 5;
+let floatingPointNumber = 5.5;
+console.log(integerNumber); 
+console.log(floatingPointNumber);
+
+const largeNumber = 1_000_000; 
+console.log(largeNumber);
+
+let scientificNotation = 1.23e5;
+console.log(scientificNotation); // Експоненціальний запис означає 1.23 * 10^5
+
+let randomNumber = Math.random();
+console.log(randomNumber);
+
+let firstDecimal = 0.11;
+let secondDecimal = 0.12;
+let sum = firstDecimal + secondDecimal; 
+console.log(sum); 
